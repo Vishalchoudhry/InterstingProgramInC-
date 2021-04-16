3973c7d257064f35ae5b0be56e0828f6880413bd
Vishal Choudhary <73791462+Vishalchoudhry@users.noreply.github.com>
2021-04-16 16:37:40 +0530

    Add files via upload

---
+++ urlopen.cpp
@@ -0,0 +1,16 @@
+/*
+Copyright©2021VishalChoudhary.
+v0.1;
+openurl program in c++
+*/
+#include<iostream>
+#include<time.h>
+int main()
+{
+    std::cout<<"\t\t\t\t|Launching Website....\n";
+    system("start  opera vishalchoudhary.itch.io");
+    _sleep(5000);
+    std::cout<<"\t\t\t\t|Please Wait....|\n";
+    std::cin.get();
+    return 0;
+};
